746f1243fc3323f610b2ec97dda7cd1c873575c6
Talles L <talleslasmar@gmail.com>
2016-07-17 12:04:51 -0300

    fixed local paths usage

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 'use strict'
 
-module.exports = (path) => {
+const path = require('path')
+
+module.exports = (p) => {
+  if (p.startsWith(`.${path.sep}`) || p.startsWith(`..${path.sep}`)) {
+    p = path.resolve(path.dirname(module.parent.filename), p)
+  }
+
   try {
-    require.resolve(path)
+    return require(p)
   } catch (err) {
     if (err.code === 'MODULE_NOT_FOUND') return undefined
     else throw err
   }
-  return require(path)
 }
-
